6e0729d2490174c7fd029aac0ab0e354d03522d3
Phil McMinn <phil@owl.local>
2022-12-05 15:30:59 +0000

    Adding first versions of all files

---
+++ code/src/test/java/uk/ac/shef/FindLastTest.java
@@ -0,0 +1,43 @@
+package uk.ac.shef;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FindLastTest {
+
+    // (a) Identify the fault: i > 0 instead of i >= 0 in the loop header
+    @Test
+    public void findLast() {
+        // anything where y is the first element in x and does not appear
+        // later in the array
+        int[] x = {1, 0};
+
+        // note that this will fail!
+        // assertThat(BuggyClass.findLast(x, 1), equalTo(0));
+
+        assertThat(FixedClass.findLast(x, 1), equalTo(0));
+    }
+
+    // (b) A JUnit test case that does not execute the fault
+    @Test
+    public void findLast_DoesNotExecuteFault() {
+        // anything that does not execute the loop test,
+        // so a null x (throws a NullPointerException before loop is executed)
+        assertThrows(NullPointerException.class, () -> {
+            BuggyClass.findLast(null, 1);
+        });
+    }
+
+    // (c) A JUnit test that executes the fault but does not cause a failure
+    @Test
+    public void testFind_LastInfectionNoFailure() {
+        // anything where y is not in x
+        // (e) the state is infected because the first index of x not considered.
+        // However, because y is not in x anyway, the method behaves correctly
+        int[] x = {0, 1};
+        assertThat(BuggyClass.findLast(x, 2), equalTo(-1));
+    }
+}
