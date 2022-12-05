e7993859391b8affe1dabc89a87f78bdbbb5e9d1
Phil McMinn <phil@owl.local>
2022-12-05 15:56:15 +0000

    Faults -> Defects

--- code/src/test/java/uk/ac/shef/FindLastTest.java
+++ code/src/test/java/uk/ac/shef/FindLastTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FindLastTest {
 
-    // (a) Identify the fault: i > 0 instead of i >= 0 in the loop header
+    // (a) Identify the defect: i > 0 instead of i >= 0 in the loop header
     @Test
     public void findLast() {
         // anything where y is the first element in x and does not appear
@@ -21,9 +21,9 @@ public class FindLastTest {
         assertThat(FixedClass.findLast(x, 1), equalTo(0));
     }
 
-    // (b) A JUnit test case that does not execute the fault
+    // (b) A JUnit test case that does not execute the defect
     @Test
-    public void findLast_DoesNotExecuteFault() {
+    public void findLast_DoesNotExecuteDefect() {
         // anything that does not execute the loop test,
         // so a null x (throws a NullPointerException before loop is executed)
         assertThrows(NullPointerException.class, () -> {
@@ -31,9 +31,9 @@ public class FindLastTest {
         });
     }
 
-    // (c) A JUnit test that executes the fault but does not cause a failure
+    // (c) A JUnit test that executes the defect but does not cause a failure
     @Test
-    public void testFind_ExecutesFaultButNoFailure() {
+    public void testFind_ExecutesDefectButNoFailure() {
         // anything where y is not in x
         // (e) the state is infected because the first index of x not considered.
         // However, because y is not in x anyway, the method behaves correctly
